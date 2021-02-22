c7e58a82da4a09435c3725c21e6f747fc2a21ed9
felice76 <felice76@hotmail.it>
2021-02-22 14:59:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+TweenMax.staggerFrom(".container", 1, {
+    top: "-100%",
+    opacity: 1,
+    ease: Expo.easeInOut
+})
+
+TweenMax.to(".logo", 1.5, {
+    top: "-50%",
+    delay: 1,
+    opacity: 1,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".nav ul li", 1.55, {
+
+    delay: 1,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut
+
+}, 0.2)
+
+TweenMax.staggerFrom(".speaker", 1.4, {
+
+    delay: 1.2,
+    opacity: 0,
+    y: 20, 
+    ease: Expo.easeInOut
+
+}, 0.4)
+
+TweenMax.staggerFrom(".music", 1.8, {
+
+    delay: 2,
+    opacity: 0,
+    y: 10, 
+    ease: Expo.easeInOut
+
+})
+
+
+
+TweenMax.staggerFrom(".item h2", 2.2, {
+    delay: 2.5,
+    opacity: 0,
+    y: 0, 
+    ease: Expo.easeInOut
+
+}, 1.5)
+
+TweenMax.staggerFrom(".item p", 2.2, {
+    delay: 3.5,
+    opacity: 0,
+    y: 0, 
+    ease: Expo.easeInOut
+
+}, 2)
+
+TweenMax.staggerFrom(".item .cta_group", 2.2, {
+    delay: 4,
+    opacity: 0,
+    y: 0, 
+    ease: Expo.easeInOut
+
+}, 3)
+
+TweenMax.staggerFrom(".left", 2.2, {
+    delay: 4,
+    opacity: 0,
+    y: -80, 
+    ease: Expo.easeInOut
+
+}, 3)
+
+TweenMax.staggerFrom(".up", 2.2, {
+    delay: 4,
+    opacity: 0,
+    x: -280, 
+    ease: Expo.easeInOut
+
+}, 3)
+
+TweenMax.staggerFrom(".center", 2.2, {
+
+    delay: 4,
+    opacity: 0,
+    y: -30, 
+    ease: Expo.easeInOut
+
+}, 1)
+
+TweenMax.staggerFrom(".down", 2.2, {
+    delay: 4,
+    opacity: 0,
+    x: -350, 
+    ease: Expo.easeInOut
+
+}, 3.5)
+
+TweenMax.staggerFrom(".footer i", 1, {
+    delay: 1,
+    opacity: 0, 
+    ease: Expo.easeInOut
+
+})
+
